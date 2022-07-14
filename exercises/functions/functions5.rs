d147c826442fdc02eda90fd7017665d1caeeda40
Freben <fschocke@gmail.com>
2022-07-14 20:25:48 +0200

    Learn Rust, finish Quiz 1

--- exercises/functions/functions5.rs
+++ exercises/functions/functions5.rs
@@ -1,13 +1,12 @@
 // functions5.rs
 // Make me compile! Execute `rustlings hint functions5` for hints :)
 
-// I AM NOT DONE
-
 fn main() {
     let answer = square(3);
     println!("The answer is {}", answer);
 }
 
 fn square(num: i32) -> i32 {
-    num * num;
+    // return  num * num; would also be possible, semicolons are then needed!
+    num * num
 }
