70f92208f00dc25c20f54394f4f1f2c6cd20e6f9
frederik.schocke <frederik.schocke@aoe.com>
2025-04-16 17:07:52 +0200

    feat: finish vecs

--- exercises/05_vecs/vecs1.rs
+++ exercises/05_vecs/vecs1.rs
@@ -3,7 +3,10 @@ fn array_and_vec() -> ([i32; 4], Vec<i32>) {
 
     // TODO: Create a vector called `v` which contains the exact same elements as in the array `a`.
     // Use the vector macro.
-    // let v = ???;
+    // let v = vec![10, 20, 30, 40]; // One simple solution
+
+    let mut v = vec![];
+    v.extend_from_slice(&a);
 
     (a, v)
 }
