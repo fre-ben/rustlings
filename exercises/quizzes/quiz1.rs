0f31d295d5734728a7cc20488e147c2e54425ecb
frederik.schocke <frederik.schocke@aoe.com>
2025-04-16 16:17:10 +0200

    feat: finish quiz1

--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -10,7 +10,15 @@
 
 // TODO: Write a function that calculates the price of an order of apples given
 // the quantity bought.
-// fn calculate_price_of_apples(???) -> ??? { ??? }
+fn calculate_price_of_apples(quantity: i32) -> i32 {
+    let mut price: i32 = 2;
+
+    if quantity > 40 {
+        price = 1;
+    }
+
+    quantity * price
+}
 
 fn main() {
     // You can optionally experiment here.
