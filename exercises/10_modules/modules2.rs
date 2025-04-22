b9a1c7182bf5014d211c04a31bac6b8ca71ff40a
frederik.schocke <frederik.schocke@aoe.com>
2025-04-22 16:37:13 +0200

    feat: finish modules

--- exercises/10_modules/modules2.rs
+++ exercises/10_modules/modules2.rs
@@ -1,10 +1,10 @@
 // You can bring module paths into scopes and provide new names for them with
 // the `use` and `as` keywords.
 
-mod delicious_snacks {
+pub mod delicious_snacks {
     // TODO: Add the following two `use` statements after fixing them.
-    // use self::fruits::PEAR as ???;
-    // use self::veggies::CUCUMBER as ???;
+    pub use self::fruits::PEAR as fruit;
+    pub use self::veggies::CUCUMBER as veggie;
 
     mod fruits {
         pub const PEAR: &str = "Pear";
