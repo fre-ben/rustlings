22d1e65ca647af662704b29823870f1857baa3c0
frederik.schocke <frederik.schocke@aoe.com>
2025-06-18 17:02:26 +0200

    feat: finish iterators3

--- exercises/18_iterators/iterators3.rs
+++ exercises/18_iterators/iterators3.rs
@@ -11,21 +11,54 @@ enum DivisionError {
 // TODO: Calculate `a` divided by `b` if `a` is evenly divisible by `b`.
 // Otherwise, return a suitable error.
 fn divide(a: i64, b: i64) -> Result<i64, DivisionError> {
-    todo!();
+    match (a, b) {
+        (_, 0) => Err(DivisionError::DivideByZero),
+        (a, b) if a == i64::MIN || b == i64::MIN => Err(DivisionError::IntegerOverflow),
+        (a, b) if a % b != 0 => Err(DivisionError::NotDivisible),
+        (a, b) => Ok(a / b),
+    }
+
+    // Alternative solution without match
+    // if b == 0 {
+    //     return Err(DivisionError::DivideByZero);
+    // }
+    // if a == i64::MIN || b == i64::MIN {
+    //     return Err(DivisionError::IntegerOverflow);
+    // }
+    // if a % b != 0 {
+    //     return Err(DivisionError::NotDivisible);
+    // }
+
+    // Ok(a / b)
 }
 
 // TODO: Add the correct return type and complete the function body.
 // Desired output: `Ok([1, 11, 1426, 3])`
-fn result_with_list() {
+fn result_with_list() -> Result<Vec<i64>, DivisionError> {
     let numbers = [27, 297, 38502, 81];
-    let division_results = numbers.into_iter().map(|n| divide(n, 27));
+    let division_results: Vec<Result<i64, DivisionError>> =
+        numbers.into_iter().map(|n| divide(n, 27)).collect();
+
+    let mut vec_result: Vec<i64> = Vec::new();
+
+    for result in division_results.into_iter() {
+        match result {
+            Ok(x) => vec_result.push(x),
+            Err(e) => return Err(e),
+        }
+    }
+
+    Ok(vec_result)
 }
 
-// TODO: Add the correct return type and complete the function body.
-// Desired output: `[Ok(1), Ok(11), Ok(1426), Ok(3)]`
-fn list_of_results() {
+// // TODO: Add the correct return type and complete the function body.
+// // Desired output: `[Ok(1), Ok(11), Ok(1426), Ok(3)]`
+fn list_of_results() -> Vec<Result<i64, DivisionError>> {
     let numbers = [27, 297, 38502, 81];
-    let division_results = numbers.into_iter().map(|n| divide(n, 27));
+    let division_results: Vec<Result<i64, DivisionError>> =
+        numbers.into_iter().map(|n| divide(n, 27)).collect();
+
+    division_results
 }
 
 fn main() {
